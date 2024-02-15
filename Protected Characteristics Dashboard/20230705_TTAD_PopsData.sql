5da0cb68acbce766a7eacd450f0683dc31ca8ce2
alxmacdnld <alxmacdnld@gmail.com>
2024-02-15 09:43:30 +0000

    Update to comissioner/provider hierarchies ref tables

--- Protected Characteristics Dashboard/20230705_TTAD_PopsData.sql
+++ Protected Characteristics Dashboard/20230705_TTAD_PopsData.sql
@@ -1,33 +1,33 @@
-/****** Script to produce populations by different protected characteristics (age, gender and ethnicity) and geographies (National, ICB and Sub-ICB) using the 2021 census data.
-		This is used in the TTAD Protected Characteristics dashboard to produce the rates of open referrals per 100,000 population values. ******/
+/* ---- Script to produce populations by different protected characteristics (age, gender and ethnicity) and geographies 
+(National, ICB and Sub-ICB) using the 2021 census data. Used to produce rates of open referrals per 100,000 population values -- */
 
-------------------------Age Group-------------------------------
+-- Age Group -----------------------------------------------------------------------------------------------------------------------------------------------
+/* This produces a base table with the population for each age grouping for each MSOA and it is matched to the Sub-ICB, ICB and Region that MSOA belongs to.
+-- Age data wasn't available at LSOA level so MSOA was used instead ---------------------------------------------------------------------------------------- */
 
---This produces a base table with the population for each age grouping for each MSOA and it is matched to the Sub-ICB, ICB and Region that MSOA belongs to.
---Age data wasn't available at LSOA level so MSOA was used instead
 IF OBJECT_ID ('[MHDInternal].[TEMP_TTAD_ProtChar_Population_Age_Base_Table]') IS NOT NULL DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Age_Base_Table]
 
 SELECT DISTINCT
-	l.MSOA21
-	,l.ODS_SubICB_Code22 AS SubICBCode
-	,r.Organisation_Name AS SubICBName
+	l.[MSOA21]
+	,l.[ODS_SubICB_Code22] AS 'SubICBCode'
+	,r.[Organisation_Name] AS 'SubICBName'
 	,l.[ICBName]
 	,l.[ICBCode]
 	,l.[Region_Name]
 	,l.[Region_Code]
-	,a.Measure_Name
+	,a.[Measure_Name]
     ,a.[Count]
 
 INTO [MHDInternal].[TEMP_TTAD_ProtChar_Population_Age_Base_Table]
 
 FROM [UKHF_Census].[Age_By_Single_Year_Of_Age_V21] a
-	INNER JOIN  [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.MSOA21= a.Geography_Code COLLATE DATABASE_DEFAULT
-	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] r ON l.ODS_SubICB_Code22=r.Organisation_Code
+	INNER JOIN  [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.[MSOA21] = a.[Geography_Code] COLLATE DATABASE_DEFAULT
+	LEFT JOIN [Reporting_UKHD_ODS].[Commissioner_Hierarchies] r ON l.[ODS_SubICB_Code22] = r.[Organisation_Code]
 	--Inner joins to a lookup table which matches LSOA 2021 codes with MSOA 2021 codes, Sub-ICB names, ICB names and Region names 
 	--so the census populations can be aggregated to Sub-ICB and ICB levels
 
-WHERE a.[Effective_Snapshot_Date] = '2021-03-21' and a.Geography_Type='msoa' and Geography_Code like'E%'
-	and Measure_Name in 
+WHERE a.[Effective_Snapshot_Date] = '2021-03-21' and a.[Geography_Type] = 'msoa' and [Geography_Code] like'E%'
+	and [Measure_Name] in 
 	('Aged 4 years and under','Aged 5 to 9 years','Aged 10 to 15 years','Aged 16 years', 'Aged 17 years', 'Aged 18 years', 'Aged 19 years','Aged 20 to 24 years',
 	'Aged 25 years','Aged 26 years','Aged 27 years','Aged 28 years','Aged 29 years','Aged 30 years','Aged 31 years','Aged 32 years','Aged 33 years','Aged 34 years',
 	'Aged 35 to 49 years','Aged 50 to 64 years','Aged 65 to 74 years','Aged 75 to 84 years','Aged 85 years and over')
@@ -52,7 +52,7 @@ SELECT
 								,'Aged 34 years','Aged 35 to 49 years','Aged 50 to 64 years') THEN '26-64'
 		WHEN [Measure_Name] IN ('Aged 65 to 74 years','Aged 75 to 84 years','Aged 85 years and over') THEN '65+'
 		END AS VARCHAR(255))
-	AS Variable
+	AS [Variable]
 	,SUM([Count]) AS [Pop]
 INTO [MHDInternal].[DASHBOARD_TTAD_ProtChar_PopsData]
 FROM [MHDInternal].[TEMP_TTAD_ProtChar_Population_Age_Base_Table]
@@ -76,9 +76,9 @@ GROUP BY
 IF OBJECT_ID ('[MHDInternal].[TEMP_TTAD_ProtChar_Population_Gender_Base_Table]') IS NOT NULL DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Gender_Base_Table] --this is the same base used for Dementia
 
 SELECT DISTINCT
-	l.LSOA21
-	,l.ODS_SubICB_Code22 AS SubICBCode
-	,r.Organisation_Name AS SubICBName
+	l.[LSOA21]
+	,l.[ODS_SubICB_Code22] AS 'SubICBCode'
+	,r.[Organisation_Name] AS 'SubICBName'
 	,l.[ICBName]
 	,l.[ICBCode]
 	,l.[Region_Name]
@@ -89,8 +89,8 @@ SELECT DISTINCT
 INTO [MHDInternal].[TEMP_TTAD_ProtChar_Population_Gender_Base_Table]
 
 FROM [UKHF_Census].[Sex1] g
-	INNER JOIN [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.LSOA21= g.Geography_Code COLLATE DATABASE_DEFAULT
-	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] r ON l.ODS_SubICB_Code22=r.Organisation_Code
+	INNER JOIN [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.[LSOA21] = g.[Geography_Code] COLLATE DATABASE_DEFAULT
+	LEFT JOIN [Reporting_UKHD_ODS].[Commissioner_Hierarchies] r ON l.[ODS_SubICB_Code22] = r.[Organisation_Code]
 	--Inner joins to a lookup table which matches LSOA 2021 codes with MSOA 2021 codes, Sub-ICB names, ICB names and Region names 
 	--so the census populations can be aggregated to Sub-ICB and ICB levels
 
@@ -111,7 +111,7 @@ SELECT
 	,CASE WHEN [Sex]='Female' THEN 'Female'
 		WHEN [Sex]='Male' THEN 'Male'
 		ELSE 'Other' END
-	AS Variable
+	AS [Variable]
 	,SUM([Count]) AS [Pop]
 
 FROM [MHDInternal].[TEMP_TTAD_ProtChar_Population_Gender_Base_Table]
@@ -133,20 +133,20 @@ GROUP BY
 IF OBJECT_ID ('[MHDInternal].[TEMP_TTAD_ProtChar_Population_Ethnicity_Base_Table]') IS NOT NULL DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Ethnicity_Base_Table] --this is the same base used for Dementia
 
 SELECT DISTINCT
-	l.LSOA21
-	,l.ODS_SubICB_Code22 AS SubICBCode
-	,r.Organisation_Name AS SubICBName
+	l.[LSOA21]
+	,l.[ODS_SubICB_Code22] AS 'SubICBCode'
+	,r.[Organisation_Name] AS 'SubICBName'
 	,l.[ICBName]
 	,l.[ICBCode]
 	,l.[Region_Name]
 	,l.[Region_Code]
-	,e.Measure
-    ,e.Measure_Value
+	,e.[Measure]
+    ,e.[Measure_Value]
 INTO [MHDInternal].[TEMP_TTAD_ProtChar_Population_Ethnicity_Base_Table]
 
 FROM [UKHF_Census].[Ethnic_Group_V21] e
-	INNER JOIN [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.LSOA21= e.Geography_Code COLLATE DATABASE_DEFAULT
-	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] r ON l.ODS_SubICB_Code22=r.Organisation_Code
+	INNER JOIN [MHDInternal].[REFERENCE_Lookup_LSOA21_MSOA21_ICB] l ON l.[LSOA21] = e.[Geography_Code] COLLATE DATABASE_DEFAULT
+	LEFT JOIN [Reporting_UKHD_ODS].[Commissioner_Hierarchies] r ON l.[ODS_SubICB_Code22] = r.[Organisation_Code]
 	--Inner joins to a lookup table which matches LSOA 2021 codes with MSOA 2021 codes, Sub-ICB names, ICB names and Region names 
 	--so the census populations can be aggregated to Sub-ICB and ICB levels
 
@@ -172,7 +172,7 @@ SELECT
 		WHEN Measure like'%White%' THEN 'White'
 		WHEN Measure  like '%Other%' THEN 'Other Ethnic Groups'
 		ELSE 'Other'
-		END AS Variable
+		END AS [Variable]
 	,ROUND(SUM(Measure_Value),0) AS [Pop]
 
 FROM [MHDInternal].[TEMP_TTAD_ProtChar_Population_Ethnicity_Base_Table]
@@ -192,8 +192,6 @@ GROUP BY
 		ELSE 'Other' END
 
 ------------------------------------------------------------------------------------------
---Drops temporary tables used in the query -----------------------------------------------
-
 DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Age_Base_Table]
 DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Gender_Base_Table]
 DROP TABLE [MHDInternal].[TEMP_TTAD_ProtChar_Population_Ethnicity_Base_Table]
