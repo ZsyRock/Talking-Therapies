ee90b88b39694a37838a0268b593ff406c940b74
alxmacdnld <alxmacdnld@gmail.com>
2024-02-16 10:19:31 +0000

    Update to comissioner hierarchies ref tables

--- Employment Support and Student Dashboard/Step 2 UDAL Employment Support Indicator FINAL 230523.sql
+++ Employment Support and Student Dashboard/Step 2 UDAL Employment Support Indicator FINAL 230523.sql
@@ -1,5 +1,3 @@
---Please note this information is experimental and it is only intended for use for management purposes.
-
 /****** Script for Employment Support Dashboard to produce the table for the employment support indicator dashboard page ******/
 --This script must be run after Employment Support Main Tables script
 
@@ -13,17 +11,14 @@ WHERE [Month] = (SELECT MAX([Month]) FROM [MHDInternal].[DASHBOARD_TTAD_EmpSupp_
 --Employment Support Indicator Base Table
 --This table produces a record level table for the refresh period defined below, as a basis for the output table produced further below ([MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator])
 
-DECLARE @PeriodStart DATE
-DECLARE @PeriodEnd DATE 
 --For refreshing, the offset for getting the period start and end should be 0 to get the latest month
-SET @PeriodStart = (SELECT DATEADD(MONTH,0,MAX([ReportingPeriodStartDate])) FROM [mesh_IAPT].[IsLatest_SubmissionID])
-SET @PeriodEnd = (SELECT eomonth(DATEADD(MONTH,0,MAX([ReportingPeriodEndDate]))) FROM [mesh_IAPT].[IsLatest_SubmissionID])
+DECLARE @PeriodStart DATE = (SELECT DATEADD(MONTH,0,MAX([ReportingPeriodStartDate])) FROM [mesh_IAPT].[IsLatest_SubmissionID])
+DECLARE @PeriodEnd DATE = (SELECT eomonth(DATEADD(MONTH,0,MAX([ReportingPeriodEndDate]))) FROM [mesh_IAPT].[IsLatest_SubmissionID])
+
 SET DATEFIRST 1
 
-PRINT @PeriodStart
-PRINT @PeriodEnd
-
 IF OBJECT_ID ('[MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]') IS NOT NULL DROP TABLE [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
+
 SELECT DISTINCT
 	CAST(DATENAME(m, l.ReportingPeriodStartDate) + ' ' + CAST(DATEPART(yyyy, l.ReportingPeriodStartDate) AS VARCHAR) AS DATE) AS Month
 	,r.Person_ID
@@ -33,7 +28,7 @@ SELECT DISTINCT
 	,CASE WHEN r.CompletedTreatment_Flag  = 'True' 
 	AND EmpSupportInd='Y' AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS FinishedTreatEmpSuppIndYes	--Flag for those who completed treatment and who are eligible for employment support
 	,CASE WHEN  r.CompletedTreatment_Flag  = 'True' 
-	AND EmpSupportInd='Y' AND ec.Count_EmpSupp>0 AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes	
+	AND EmpSupportInd='Y' AND ec.Count_EmpSupp > 0 AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes	
 	--Flag for those who completed treatment, who are eligible for employment support and have had at least one employment support appointment
 		
 	,CASE WHEN EmpSupportInd='Y' AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS EmpSuppIndYes	--Flag for those who are eligible for employment support
@@ -62,25 +57,21 @@ FROM [mesh_IAPT].[IDS101referral] r
 	LEFT JOIN [MHDInternal].[TEMP_TTAD_EmpSupp_EmpSuppCount] ec ON ec.PathwayID=r.PathwayID
 
 	LEFT JOIN [Internal_Reference].[ComCodeChanges] cc ON r.OrgIDComm = cc.Org_Code COLLATE database_default
-	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] ch ON COALESCE(cc.New_Code, r.OrgIDComm) = ch.Organisation_Code COLLATE database_default 
-		AND ch.Effective_To IS NULL
+	LEFT JOIN [Reporting_UKHD_ODS].[Commissioner_Hierarchies] ch ON COALESCE(cc.New_Code, r.OrgIDComm) = ch.Organisation_Code COLLATE database_default AND ch.Effective_To IS NULL
 	LEFT JOIN [Internal_Reference].[Provider_Successor] ps ON r.OrgID_Provider = ps.Prov_original COLLATE database_default
-	LEFT JOIN [Reporting].[Ref_ODS_Provider_Hierarchies_ICB] ph ON COALESCE(ps.Prov_Successor, r.OrgID_Provider) = ph.Organisation_Code COLLATE database_default
-		AND ph.Effective_To IS NULL
-	--Four tables for getting the up-to-date Sub-ICB/ICB/Region/Provider names/codes
-WHERE r.UsePathway_Flag = 'True' 
-	AND l.IsLatest = 1	--To get the latest data
-	AND l.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, -1, @PeriodStart) AND @PeriodStart	--for monthly refresh the offset should be -1 as we want the data for the latest 2 months month (i.e. to refresh the previous month's primary data)
-	AND emp.EmpSupportInd='Y'	--Only looking at those who are eligible for employment support
-	AND r.ServDischDate BETWEEN l.ReportingPeriodStartDate AND l.ReportingPeriodEndDate	--Only looking at discharges within the reporting period
+	LEFT JOIN [Reporting_UKHD_ODS].[Provider_Hierarchies] ph ON COALESCE(ps.Prov_Successor, r.OrgID_Provider) = ph.Organisation_Code COLLATE database_default AND ph.Effective_To IS NULL
 
+WHERE r.UsePathway_Flag = 'True' 
+	AND l.IsLatest = 1	
+	AND l.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, -1, @PeriodStart) AND @PeriodStart
+	AND emp.EmpSupportInd='Y'
+	AND r.ServDischDate BETWEEN l.ReportingPeriodStartDate AND l.ReportingPeriodEndDate	--Only looking at discharges within the reporting period
 
 --Employment Support Indicator Output Table
 --This table sums the flags produced in the base table above. 
 
-------------------
---IF OBJECT_ID ('[MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]') IS NOT NULL DROP TABLE [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
 INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
+
 SELECT
 	Month
 	,RegionNameComm
@@ -97,8 +88,9 @@ SELECT
 	,SUM(FinishedTreatEmpSuppIndYes) as FinishedTreatEmpSuppIndYes
 	,SUM(EmpSuppFirstAppAndEmpSuppIndYes) as EmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(EmpSuppIndYes) as EmpSuppIndYes
---INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
+
 FROM [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
+
 GROUP BY 
 	Month
 	,RegionNameComm
@@ -112,6 +104,8 @@ GROUP BY
 	,ProviderCode
 	,ProviderName
 
---Drop temporary tables created to produce the final output table
+--Drop temporary tables ------------------------------------
 DROP TABLE [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
 DROP TABLE [MHDInternal].[TEMP_TTAD_EmpSupp_EmpSuppCount]
+-------------------------------------------------------------------
+PRINT 'Updated - [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]'
